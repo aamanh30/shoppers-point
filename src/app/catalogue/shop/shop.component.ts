391e5075c1599ed782ba909bd8410c4813213e2f
aamanh30 <aamanh30@gmail.com>
2023-06-19 02:36:01 +0530

    updated the necessary components to filter the products in the catalogue page as per the selected filters

--- src/app/catalogue/shop/shop.component.ts
+++ src/app/catalogue/shop/shop.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { EMPTY, Observable } from 'rxjs';
-import { Product } from '../../shared/models';
 import { Store } from '@ngrx/store';
+import { Product } from '../../shared/models';
 import {
   CatalogueActions,
   CatalogueFeature,
   CatalogueSelectors
 } from '../../catalogue-state';
-import { Router } from '@angular/router';
 import { CartActions } from '../../cart-state';
 import { CartAction } from '../../cart-state/models';
 import {
   FilterType,
   CatalogueFilter,
-  Range
+  CatalogueFilters,
+  Range,
+  FilterEvent,
+  CatalogueFilterKey
 } from '../../catalogue-state/models';
 
 @Component({
@@ -22,11 +25,12 @@ import {
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  readonly FilterType = FilterType;
   products$: Observable<Product[]> = EMPTY;
   categories$: Observable<CatalogueFilter[]> = EMPTY;
   ratings$: Observable<CatalogueFilter[]> = EMPTY;
   range$: Observable<Range> = EMPTY;
-  readonly FilterType = FilterType;
+  filters$: Observable<CatalogueFilters | undefined> = EMPTY;
   productsPerPage = 5;
   constructor(
     private store: Store<CatalogueFeature.CataloguePartialState>,
@@ -38,6 +42,7 @@ export class ShopComponent implements OnInit {
     this.categories$ = this.store.select(CatalogueSelectors.categories);
     this.ratings$ = this.store.select(CatalogueSelectors.ratings);
     this.range$ = this.store.select(CatalogueSelectors.range);
+    this.filters$ = this.store.select(CatalogueSelectors.filters);
     this.store.dispatch(CatalogueActions.fetchProducts());
   }
 
@@ -61,5 +66,19 @@ export class ShopComponent implements OnInit {
 
   onViewProduct(productId: number): void {
     this.router.navigate([`/product-details/${productId}`]);
+  }
+
+  onFiltersChanged(
+    { label, checked, value }: FilterEvent,
+    key: CatalogueFilterKey
+  ): void {
+    this.store.dispatch(
+      CatalogueActions.setFilters({
+        label,
+        checked,
+        value,
+        key
+      })
+    );
   }
 }
