8e86b28a64420305881e92ae8fcbe3763aa55c55
aamanh30 <aamanh30@gmail.com>
2023-06-26 00:50:24 +0530

    renamed billing form component to address form component

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutDetailsComponent } from './checkout-details/checkout-details.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { BillingFormComponent } from './billing-form/billing-form.component';
 import { Route, RouterModule } from '@angular/router';
 import { CatalogueStateModule } from '../catalogue-state';
+import { CartStateModule } from '../cart-state';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { FormlyModule } from '@ngx-formly/core';
+import { CheckoutStateModule } from '../checkout-state/checkout-state.module';
+import { AddressFormComponent } from './address-form/address-form.component';
 
 const routes: Route[] = [
   {
@@ -17,8 +22,17 @@ const routes: Route[] = [
   declarations: [
     CheckoutDetailsComponent,
     OrderSummaryComponent,
-    BillingFormComponent
+    AddressFormComponent
   ],
-  imports: [CommonModule, RouterModule.forChild(routes), CatalogueStateModule]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    CheckoutStateModule,
+    CatalogueStateModule,
+    ReactiveFormsModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot(),
+    CartStateModule
+  ]
 })
 export class CheckoutModule {}
