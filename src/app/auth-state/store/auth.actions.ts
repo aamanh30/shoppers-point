adae13c81376177b7ff5e086cd5968ffa0a7499a
aamanh30 <aamanh30@gmail.com>
2023-07-02 00:03:58 +0530

    added the necessary files for state, models and services for AuthStateModule

---
+++ src/app/auth-state/store/auth.actions.ts
@@ -0,0 +1,50 @@
+import { createAction, props } from '@ngrx/store';
+import { AuthForm } from '../models/auth-form';
+
+export enum AuthActionTypes {
+  SignUp = '[Auth] Sign Up',
+  SignUpSuccess = '[Auth] Sign Up Success',
+  SignIn = '[Auth] Sign In',
+  SignInSuccess = '[Auth] Sign In Success',
+  ForgotPassword = '[Auth] Forgot Password',
+  ForgotPasswordSuccess = '[Auth] Forgot Password Success',
+  ResetPassword = '[Auth] Reset Password',
+  ResetPasswordSuccess = '[Auth] Reset Password Success',
+  AuthError = '[Auth] Auth Error'
+}
+
+export const signUp = createAction(AuthActionTypes.SignUp, props<AuthForm>());
+
+export const signUpSuccess = createAction(AuthActionTypes.SignUpSuccess);
+
+export const signIn = createAction(
+  AuthActionTypes.SignIn,
+  props<Partial<AuthForm>>()
+);
+
+export const signInSuccess = createAction(AuthActionTypes.SignInSuccess);
+
+export const forgotPassword = createAction(
+  AuthActionTypes.ForgotPassword,
+  props<{ email: string }>()
+);
+
+export const forgotPasswordSuccess = createAction(
+  AuthActionTypes.ForgotPasswordSuccess
+);
+
+export const resetPassword = createAction(
+  AuthActionTypes.ResetPassword,
+  props<{ email: string; oldPassword: string; password: string }>()
+);
+
+export const resetPasswordSuccess = createAction(
+  AuthActionTypes.ResetPasswordSuccess
+);
+
+export const authError = createAction(
+  AuthActionTypes.AuthError,
+  props<{
+    error: Partial<Error>;
+  }>()
+);
