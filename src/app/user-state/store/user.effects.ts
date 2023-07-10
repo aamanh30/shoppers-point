cc95732660375094446d286bc1e5924ba6cd5179
aamanh30 <aamanh30@gmail.com>
2023-07-11 00:27:55 +0530

    fixed the token bug

--- src/app/user-state/store/user.effects.ts
+++ src/app/user-state/store/user.effects.ts
@@ -3,7 +3,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { concatMap, map, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
-import { fetchError, fetchUser, fetchUserSuccess } from './user.actions';
+import {
+  clearUser,
+  fetchError,
+  fetchUser,
+  fetchUserSuccess
+} from './user.actions';
 import { UserService } from '../services/user/user.service';
 import { environment } from '../../../environments/environment';
 import { User } from '../../shared/models';
@@ -27,10 +32,27 @@ export class UserEffects {
       this.actions$.pipe(
         ofType(fetchUserSuccess),
         map(user => {
-          localStorage.setItem(environment.assessTokenKey, user.accessToken);
+          localStorage.setItem(
+            environment.assessTokenKey,
+            user.accessToken ?? user.stsTokenManager.accessToken
+          );
           this.router.navigate([
             this.route.snapshot.queryParams['url'] ?? '/shop'
           ]);
+        })
+      ),
+    {
+      dispatch: false
+    }
+  );
+
+  clearUser$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(clearUser),
+        map(() => {
+          localStorage.removeItem(environment.assessTokenKey);
+          this.router.navigate(['/shop']);
         })
       ),
     {
