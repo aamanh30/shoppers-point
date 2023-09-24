4d422d8049420bc59e201d1b2956edbd558c05f2
aamanh30 <aamanh30@gmail.com>
2023-09-24 11:01:44 +0530

    updated the methods to be private and added tests

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -14,20 +14,20 @@ export class ForgotPasswordComponent {
   form: UntypedFormGroup = new UntypedFormGroup({});
   model: AuthForm | undefined;
   fields: FormlyFieldConfig[] = getForgotPasswordFieldsConfig(
-    this.onForgotPassword.bind(this),
-    this.onReset.bind(this)
+    this.#onForgotPassword.bind(this),
+    this.#onReset.bind(this)
   );
 
   constructor(private store: Store) {}
 
-  onForgotPassword(): void {
+  #onForgotPassword(): void {
     if (this.form.invalid) {
       return;
     }
     this.store.dispatch(AuthActions.forgotPassword(this.form.value));
   }
 
-  onReset(): void {
+  #onReset(): void {
     this.form.reset();
   }
 }
