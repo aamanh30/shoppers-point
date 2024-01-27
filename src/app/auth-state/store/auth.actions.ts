d1327de215e6bd8a7c9aa421126687f5f5ab443c
aamanh30 <aamanh30@gmail.com>
2024-01-27 22:48:25 +0530

    refactor(auth-state) - multiple files - Updated the state files to use `createActionGroup` instead of `createAction`

--- src/app/auth-state/store/auth.actions.ts
+++ src/app/auth-state/store/auth.actions.ts
@@ -1,56 +1,26 @@
-import { createAction, props } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 import { AuthForm } from '../models/auth-form';
+import { AUTH_KEY } from './auth-key';
 
-export enum AuthActionTypes {
-  SignUp = '[Auth] Sign Up',
-  SignUpSuccess = '[Auth] Sign Up Success',
-  SignIn = '[Auth] Sign In',
-  SignInSuccess = '[Auth] Sign In Success',
-  ForgotPassword = '[Auth] Forgot Password',
-  ForgotPasswordSuccess = '[Auth] Forgot Password Success',
-  ResetPassword = '[Auth] Reset Password',
-  ResetPasswordSuccess = '[Auth] Reset Password Success',
-  SignOut = '[Auth] Sign Out',
-  FetchUser = '[Auth] Fetch User',
-  AuthError = '[Auth] Auth Error'
-}
-
-export const signUp = createAction(AuthActionTypes.SignUp, props<AuthForm>());
-
-export const signUpSuccess = createAction(AuthActionTypes.SignUpSuccess);
-
-export const signIn = createAction(
-  AuthActionTypes.SignIn,
-  props<Partial<AuthForm>>()
-);
-
-export const signInSuccess = createAction(AuthActionTypes.SignInSuccess);
-
-export const forgotPassword = createAction(
-  AuthActionTypes.ForgotPassword,
-  props<{ email: string }>()
-);
-
-export const forgotPasswordSuccess = createAction(
-  AuthActionTypes.ForgotPasswordSuccess
-);
-
-export const resetPassword = createAction(
-  AuthActionTypes.ResetPassword,
-  props<{ email: string; oldPassword: string; password: string }>()
-);
-
-export const resetPasswordSuccess = createAction(
-  AuthActionTypes.ResetPasswordSuccess
-);
-
-export const authError = createAction(
-  AuthActionTypes.AuthError,
-  props<{
-    error: Partial<Error>;
-  }>()
-);
-
-export const signOut = createAction(AuthActionTypes.SignOut);
-
-export const fetchUser = createAction(AuthActionTypes.FetchUser);
+export const AuthActions = createActionGroup({
+  source: AUTH_KEY,
+  events: {
+    signUp: props<AuthForm>(),
+    signUpSuccess: emptyProps(),
+    signIn: props<Partial<AuthForm>>(),
+    signInSuccess: emptyProps(),
+    forgotPassword: props<{ email: string }>(),
+    forgotPasswordSuccess: emptyProps(),
+    resetPassword: props<{
+      email: string;
+      oldPassword: string;
+      password: string;
+    }>(),
+    resetPasswordSuccess: emptyProps(),
+    authError: props<{
+      error: Partial<Error>;
+    }>(),
+    signOut: emptyProps(),
+    fetchUser: emptyProps()
+  }
+});
