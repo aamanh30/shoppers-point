5d78c3ec6cc3fe6ef32f0a0aefb46dd8afb475c8
aamanh30 <aamanh30@gmail.com>
2024-08-15 23:28:32 +0530

    added expand/collapse all directive

---
+++ src/app/shared/directives/expand-collapse-all/expand-collapse-all.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ExpandCollapseAllDirective } from './expand-collapse-all.directive';
+
+describe('ExpandCollapseAllDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ExpandCollapseAllDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/shared/directives/expand-collapse-all/expand-collapse-all.directive.ts
@@ -0,0 +1,93 @@
+import {
+  AfterViewInit,
+  ContentChildren,
+  Directive,
+  ElementRef,
+  QueryList,
+  Renderer2
+} from '@angular/core';
+import { NgbAccordionDirective } from '@ng-bootstrap/ng-bootstrap';
+import { AccordionComponent } from 'ngx-bootstrap/accordion';
+
+@Directive({
+  selector: '[confusedDeveloperSharedExpandCollapseAll]'
+})
+export class ExpandCollapseAllDirective implements AfterViewInit {
+  @ContentChildren(NgbAccordionDirective)
+  ngbAccordions: QueryList<NgbAccordionDirective> | undefined;
+
+  @ContentChildren(AccordionComponent) accordions:
+    | QueryList<AccordionComponent>
+    | undefined;
+  constructor(private elemRef: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    const accodions = [
+      ...(this.ngbAccordions?.toArray() ?? []),
+      ...(this.accordions?.toArray() ?? [])
+    ];
+    if (!accodions.length) {
+      throw new Error('No accordions found');
+    }
+    const spanElement = this.#createElement('span', '', '', undefined);
+
+    this.renderer.appendChild(
+      spanElement,
+      this.#createElement(
+        'button',
+        'expand all',
+        'btn primary',
+        this.expandAll.bind(this)
+      )
+    );
+
+    this.renderer.appendChild(
+      spanElement,
+      this.#createElement(
+        'button',
+        'collapse all',
+        'btn secondary',
+        this.collapseAll.bind(this)
+      )
+    );
+
+    this.renderer.insertBefore(
+      this.elemRef.nativeElement.parentNode,
+      spanElement,
+      this.elemRef.nativeElement
+    );
+  }
+
+  expandAll(): void {
+    this.accordions?.toArray().forEach(accordion => {
+      accordion['groups'].forEach(group => (group.isOpen = true));
+    });
+    this.ngbAccordions
+      ?.toArray()
+      .forEach(ngbAccordion => ngbAccordion.expandAll());
+  }
+
+  collapseAll(): void {
+    this.accordions?.toArray().forEach(accordion => {
+      accordion['groups'].forEach(group => (group.isOpen = false));
+    });
+    this.ngbAccordions
+      ?.toArray()
+      .forEach(ngbAccordion => ngbAccordion.collapseAll());
+  }
+
+  #createElement(
+    elementType: string,
+    textContent = '',
+    classList = '',
+    onClick: ((event: Event) => void) | undefined
+  ): HTMLElement {
+    const element = this.renderer.createElement(elementType);
+    if (classList) element.classList.add(...classList.split(' '));
+    if (textContent) element.textContent = textContent;
+    if (element instanceof HTMLButtonElement) element.type = 'button';
+    if (onClick) element.onclick = onClick;
+
+    return element;
+  }
+}
